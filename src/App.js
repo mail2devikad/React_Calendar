8ce711e6108bd9086eb15a709c32f3800124f2f9
mail2devikad <mail2devikad@gmail.com>
2024-06-07 23:37:14 +0530

    react calendar functionality almost done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import Calendar from "react-calendar";
+import "./App.css";
+import { useState } from "react";
+import "react-calendar/dist/Calendar.css";
+import moment from "moment";
+import axios from "axios";
 
 function App() {
+  const [value, setValue] = useState(new Date());
+  const [events, setEvents] = useState([]);
+  const handleEvents = async (e, name) => {
+    if (e) {
+      let searchDate = "";
+      if (name == "day") {
+        searchDate = moment(e).format("YYYY/MM/DD");
+      } else {
+        searchDate = moment(e).format("YYYY/MM");
+      }
+      const { data } = await axios.get(
+        `http://calapi.inadiutorium.cz/api/v0/en/calendars/default/${searchDate}`
+      );
+
+      if (Array.isArray(data)) {
+        let newEvents = [];
+        data?.forEach((item) => {
+          newEvents.push(...item.celebrations);
+        });
+        setEvents([...newEvents]);
+      } else {
+        setEvents([...data.celebrations]);
+      }
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 py-10 grid grid-cols-2 gap-2">
+      <div className="container mx-60 my-24">
+        <Calendar
+          onChange={(e) => handleEvents(e, "day")}
+          value={value}
+          onClickMonth={(e) => handleEvents(e, "month")}
+          onclik
+        />
+      </div>
+      <div className="container bg-white lg:w-96 md:w-72 w-60 my-24">
+        <div className="mx-5 overflow-y-auto">
+          <h1 className="text-xl font-bold my-8 ">Events</h1>
+          {events.map((item, index) => (
+            <p key={index} className="border-b-2 my-1">
+              {item.title}
+            </p>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
